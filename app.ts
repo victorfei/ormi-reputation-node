109aa914abaa46205a2b5693887652e75d7955c8
Victor Fei <vjhfei@gmail.com>
2022-03-29 08:15:09 -0700

    Heroku

--- app.ts
+++ app.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from 'express';
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.get('/data', (req: Request, res: Response) => {
   const id: string = req.query.id?.toString();
